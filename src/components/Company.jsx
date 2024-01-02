603ecd0e5c50326575af0c4653cbd706e50f4066
Dev <devvarshney11@gmail.com>
2024-01-02 14:08:13 +0530

    Create a company table and change some routing

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,13 +1,97 @@
-import React from 'react'
-import Header from './Header'
-import { Box } from '@mui/material'
+import React from "react";
+import Header from "./Header";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../theme";
+import { companyMockData } from "../constants";
 
 const Company = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: "companyID", headerName: "Company ID" ,editable: true },
+    { field: "companyName", headerName: "Company Name", flex: 2 ,editable: true },
+    { field: "companyDescription", headerName: "Company Description", flex: 2 ,editable: true},
+    {
+      field: "companyEstablishment",
+      headerName: "Establishment Year",
+      flex: 1,
+      editable: true
+    },
+    { field: "companyWebsite", headerName: "Website", flex: 2 ,editable: true},
+    { field: "companyAddDate", headerName: "Added Date", flex: 2,editable: true },
+    { field: "className", headerName: "Company Type", flex: 2,editable: true },
+    { field: "userName", headerName: "Added By", flex: 2 ,editable: true},
+    {
+      field: "userType",
+      headerName: "User Type",
+      flex: 2,
+      editable: true,
+      renderCell: ({ row: { userType } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              userType === "Super Admin"
+                ? colors.greenAccent[600]
+                : userType === "Admin"
+                ? colors.greenAccent[700]:null
+            }
+            borderRadius="4px"
+          >
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {userType}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
   return (
-    <Box m = "20px">
-      <Header title="Companies"/>
+    <Box m="20px">
+      <Box
+        m="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <Header title="Companies" />
+        <DataGrid
+          checkboxSelection
+          rows={companyMockData}
+          columns={columns}
+          getRowId={(row) => row.companyID}
+        />
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
