3b06c45d4480c8581fcb9929ad11d181e9e2d9b8
Yash-Var <yashvarshney7011@gmail.com>
2024-01-13 15:00:28 +0530

    fix some bug

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -19,7 +19,6 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
             {title}
           </Typography>
         </Box>
-        <Box>{/* <ProgressCircle progress={progress} /> */}</Box>
       </Box>
       <Box display="flex" justifyContent="space-between" mt="2px">
         <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
