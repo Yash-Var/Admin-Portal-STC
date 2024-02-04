03d4d05a870d8e2c1e2ddb9445b93b87a0b548cc
Yash-Var <yashvarshney7011@gmail.com>
2024-02-04 15:46:46 +0530

    logout

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -8,6 +8,7 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 const Topbar = () => {
   const theme = useTheme();
@@ -35,7 +36,7 @@ const Topbar = () => {
           <SettingsOutlinedIcon />
         </IconButton> */}
         <IconButton onClick={handleLogout}>
-          <PersonOutlinedIcon />
+          {localStorage.getItem("token") ? <LogoutOutlinedIcon /> : ""}
         </IconButton>
       </Box>
     </Box>
